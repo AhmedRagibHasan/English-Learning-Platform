f6d5735d48ff6686bb1e2344c41dedb69b4fb8b6
AhmedRagibHasan <ahmedragibhasan7@gmail.com>
2026-05-10 20:55:16 +0600

    Lessons Buttons ready

--- script/index.js
+++ script/index.js
@@ -1,11 +1,60 @@
 // console.log("I'm JS")
 
 const loadLessons = () => {
-    const url = "https://openapi.programming-hero.com/api/levels/all"
+    // const url = "https://openapi.programming-hero.com/api/levels/all"
 
-    fetch("url")
-    .then((res)=> res.json) //promise of json data
-    .then((json) => console.log(json));
+    fetch("https://openapi.programming-hero.com/api/levels/all")
+        .then((res) => res.json()) //promise of json data
+        .then((json) => displayLesson(json.data));
 };
 
-loadLessons()
+const loadLevelWord = (id) => {
+    
+    const url = `https://openapi.programming-hero.com/api/level/${id}`
+
+    fetch(url)
+    .then(res => res.json())
+    .then(data => console.log(data))
+}
+
+const displayLesson = (lessons) => {
+    // console.log(lessons);
+
+
+    //1.get the container
+
+    const levelContainer = document.getElementById("level-container");
+
+    levelContainer.innerHTML = ""
+
+    //2.get into every lessons
+
+    for (let lesson of lessons) {
+
+        //3.create html element
+
+        const btnDiv = document.createElement("div");
+        btnDiv.innerHTML = `<button onclick="loadLevelWord( ${lesson.level_no})" href="" class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}</button>`
+
+
+        //4.append into container
+
+
+
+
+        levelContainer.append(btnDiv);
+
+    }
+
+
+
+
+
+
+
+};
+
+
+
+
+loadLessons();
