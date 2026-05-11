f2f90b66e5a20c9cab000b88c13b746f7ce63208
AhmedRagibHasan <ahmedragibhasan7@gmail.com>
2026-05-11 21:53:44 +0600

    Designed Modal, Added values from API so be showed dynamically in Modals

--- readme.md
+++ readme.md
@@ -1,1 +1,19 @@
 [Live Link] (https://ahmedragibhasan.github.io/English-Learning-Platform/)
+
+HTML CSS(Tailwind CSS) JS 
+
+Tailwind CSS 
+
+DaisyUI Fontawesome GoogleFonts
+
+JS Learnings :
+
+Fetch API and Display in HTML
+
+To show an API in site 2 functions are use 1.load 2.display
+
+Conditional Rendering 
+
+
+
+
--- script/index.js
+++ script/index.js
@@ -31,6 +31,57 @@ const loadLevelWord = (id) => {
         });
 }
 
+const loadWordDetail = async(id) => {
+    const url = `https://openapi.programming-hero.com/api/word/${id}`
+    // console.log(url);
+    const res = await fetch(url);
+    const details = await res.json();
+    // console.log(details);
+    displayWordDetails(details.data);
+
+};
+
+const displayWordDetails = (word) => {
+    // console.log(word);
+
+    const detailBox = document.getElementById("details-container");
+
+    detailBox.innerHTML = `<div class="">
+                    <h2 class="text-2xl font-bold">
+                       ${word.word} (<i class="fa-solid fa-microphone-lines"></i>   :${word.pronunciation})
+                    </h2>
+
+                </div>
+
+                <div class="">
+                    <h2 class="font-bold">Meaning</h2>
+                    <p>${word.meaning}</p>
+                </div>
+                 
+                <div class="">
+                    <h2 class="font-bold">Example</h2>
+                    <p>${word.sentence}</p>
+                </div>
+
+                <div class="">
+                    <h2 class="font-bold">Synonyms</h2>
+                    <span class="btn">Syn1</span>
+                    <span class="btn">Syn1</span>
+                    <span class="btn">Syn1</span>
+                </div>`
+
+    document.getElementById("word_modal").showModal();
+
+
+
+
+
+
+
+}
+
+
+
 
 const displayLevelWord = (words) => {
     // console.log(words);
@@ -69,7 +120,7 @@ const displayLevelWord = (words) => {
             <p class="font-semibold ">meaning/Pronounciation</p>
             <div class="font-medium text-2xl font-bangla">"${word.meaning ? word.meaning : "অর্থ পাওয়া যায় নি"} / ${word.pronunciation? word.pronunciation : "উচ্চারন দেওয়া নেই" }"</div>
             <div class="flex justify-between items-center">
-                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+                <button onclick="loadWordDetail(${word.id})" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
                 <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
 
             </div>
